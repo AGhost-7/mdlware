00dd619028317a1f1a56d74c0b65bfb528344d9a
AGhost-7 <jonathan.boudreau.92@gmail.com>
2018-06-25 15:50:52 -0400

    Move over to standardjs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var events = require('events')
 
 var EventEmitter = typeof events === 'function' ? events : events.EventEmitter
@@ -7,8 +6,8 @@ var timers = require('timers')
 var slice = Array.prototype.slice
 
 function Dispatcher() {
-	this._stack = []
-	EventEmitter.call(this)
+  this._stack = []
+  EventEmitter.call(this)
 }
 
 Dispatcher.prototype = Object.create(null)
@@ -18,51 +17,50 @@ util.inherits(Dispatcher, EventEmitter)
 var proto = Dispatcher.prototype
 
 proto.handle = function(namespace) {
-	var stack = this._stack
-	var args = slice.call(arguments, 1)
-	var i = 0
-	var self = this
+  var stack = this._stack
+  var args = slice.call(arguments, 1)
+  var i = 0
+  var self = this
 
-	var process = function() {
-		for(; i < stack.length; i++) {
-			var item = stack[i]
-			if(item.namespace === null || item.namespace === namespace) {
-				try {
-					item.handler.apply(null, args.concat(next))
-				} catch(err) {
-					self.emit('error', err, args)
-				}
-				i++
-				break
-			}
-		}
-	}
+  var process = function() {
+    for (; i < stack.length; i++) {
+      var item = stack[i]
+      if (item.namespace === null || item.namespace === namespace) {
+        try {
+          item.handler.apply(null, args.concat(next))
+        } catch (err) {
+          self.emit('error', err, args)
+        }
+        i++
+        break
+      }
+    }
+  }
 
-	var next = function(err) {
-		timers.setImmediate(process)
-	}
+  var next = function() {
+    timers.setImmediate(process)
+  }
 
-	next()
+  next()
 }
 
 function DispatcherItem(namespace, handler) {
-	this.namespace = namespace
-	this.handler = handler
+  this.namespace = namespace
+  this.handler = handler
 }
 
 proto.use = function() {
-	if(arguments.length === 1) {
-		this._stack.push(new DispatcherItem(null, arguments[0]))
-	} else {
-		var namespace = arguments[0]
-		for(var i = 1; i < arguments.length; i++ ) {
-			this._stack.push(new DispatcherItem(namespace, arguments[i]))
-		}
-	}
-	return this
+  if (arguments.length === 1) {
+    this._stack.push(new DispatcherItem(null, arguments[0]))
+  } else {
+    var namespace = arguments[0]
+    for (var i = 1; i < arguments.length; i++) {
+      this._stack.push(new DispatcherItem(namespace, arguments[i]))
+    }
+  }
+  return this
 }
 
-
 module.exports = function() {
-	return new Dispatcher()
+  return new Dispatcher()
 }
